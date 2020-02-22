6fff11b9998deef30155e4c387e38460050a0c14
BruceZhang <zhw54110@163.com>
2020-02-22 21:02:03 +0800

    first init

---
+++ zhw-free-demo/src/main/java/com/zhw/free/zhwfreedemo/ZhwFreeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhw.free.zhwfreedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhwFreeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhwFreeDemoApplication.class, args);
+    }
+
+}
