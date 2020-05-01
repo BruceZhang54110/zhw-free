b53a990a912cd272658c65ee74489d6d2213bc31
BruceZhang <zhw54110@163.com>
2020-05-01 16:30:18 +0800

    集成redis集群

--- zhw-free-demo/src/main/java/com/zhw/free/zhwfreedemo/TestController.java
+++ zhw-free-demo/src/main/java/com/zhw/free/zhwfreedemo/TestController.java
@@ -1,8 +1,23 @@
 package com.zhw.free.zhwfreedemo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class TestController {
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
+
+    @RequestMapping("test")
+    public String test() {
+        redisTemplate.opsForValue().set("testaaa", "test zhw");
+        log.info((String) redisTemplate.opsForValue().get("testaaa"));
+        return "success";
+    }
 }
