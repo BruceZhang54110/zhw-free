7040211130dfc7ace11481f715645895543be04f
BruceZhang <zhw54110@163.com>
2021-01-19 22:22:20 +0800

    添加mybatis jar

--- zhw-free-demo/src/main/java/com/zhw/free/zhwfreedemo/TestController.java
+++ zhw-free-demo/src/main/java/com/zhw/free/zhwfreedemo/TestController.java
@@ -1,10 +1,15 @@
 package com.zhw.free.zhwfreedemo;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zhw.free.zhwfreedemo.eneity.User;
+import com.zhw.free.zhwfreedemo.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -13,11 +18,19 @@ public class TestController {
     @Autowired
     RedisTemplate redisTemplate;
 
+    @Autowired
+    private UserMapper userMapper;
+
 
     @RequestMapping("test")
     public String test() {
-        redisTemplate.opsForValue().set("testaaa", "test zhw");
-        log.info((String) redisTemplate.opsForValue().get("testaaa"));
+        // redisTemplate.opsForValue().set("testaaa", "test zhw");
+        // log.info((String) redisTemplate.opsForValue().get("testaaa"));
+        QueryWrapper<User> wrapper = new QueryWrapper<User>();
+        List<User> users = userMapper.selectList(wrapper);
+        for (User o : users) {
+            log.info(o.getEmail());
+        }
         return "success";
     }
 }
