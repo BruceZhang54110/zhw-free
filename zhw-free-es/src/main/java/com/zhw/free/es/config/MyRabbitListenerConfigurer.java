311b84b1c3e40cfc64f5abe23889ab1490c861da
BruceZhang54110 <zhw54110@163.com>
2021-07-29 18:17:44 +0800

    提交 mq 测试

---
+++ zhw-free-es/src/main/java/com/zhw/free/es/config/MyRabbitListenerConfigurer.java
@@ -0,0 +1,36 @@
+package com.zhw.free.es.config;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+
+@Configuration
+public class MyRabbitListenerConfigurer implements RabbitListenerConfigurer{
+
+    //以下配置RabbitMQ消息服务
+    @Autowired
+    public ConnectionFactory connectionFactory;
+
+
+    @Bean
+    public DefaultMessageHandlerMethodFactory myHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        // 这里的转换器设置实现了 通过 @Payload 注解 自动反序列化message body
+        factory.setMessageConverter(new MappingJackson2MessageConverter());
+        return factory;
+    }
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar rabbitListenerEndpointRegistrar) {
+        rabbitListenerEndpointRegistrar.setMessageHandlerMethodFactory(myHandlerMethodFactory());
+    }
+
+}
